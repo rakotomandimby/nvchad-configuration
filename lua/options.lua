ed3354fb60c3ab76284e3f1f4b6c2efc24a02399
Mihamina <mihamina.rakotomandimby@rktmb.org>
2024-10-03 14:49:44 +0300

    clean again

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,12 @@ require "nvchad.options"
 
 local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorline = true
+o.cursorcolumn = true
+
+local a = vim.api
+a.nvim_command("au BufNewFile,BufRead *.ejs set filetype=html")
+a.nvim_command("au BufNewFile,BufRead *.ejs LspStart html")
+a.nvim_command("au BufNewFile,BufRead *.ejs Copilot enable")
+
+
