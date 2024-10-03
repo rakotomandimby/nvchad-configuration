cfbba46e930fc3f7f3b667519b785584e2b52850
Mihamina <mihamina.rakotomandimby@rktmb.org>
2024-10-03 11:01:22 +0300

    LazyGit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,14 +23,28 @@ return {
       require "configs.lspconfig"
     end,
   },
-  { "f-person/git-blame.nvim", event = "VeryLazy"}
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  { "f-person/git-blame.nvim", event = "VeryLazy"},
+
+  -- install LazyGit
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  }
 }
+
